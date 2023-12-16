e8077992a143a2b7bf2ccc94228d82ef94b5232d
presingi_govind_raju <presingigovindraju333@gmail.com>
2023-12-16 18:05:32 +0530

    commented

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = ()=>{
     return (<div className="footer">
            <p>2023 Presingi govind raju feel free to copy</p>
-           <a href="#">LinkedIn</a>
+           {/* <a href="#">LinkedIn</a> */}
            <a href="#">FaceBook</a>
          </div>)
 }
